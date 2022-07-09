a4a562287960ee7050b44fd9fd952ee04b2ef028
Ghassan Al-Mahameed <ghassanmah@gmail.com>
2022-07-10 02:17:18 +0300

    added the ability to add users

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AdminCon;
 use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
